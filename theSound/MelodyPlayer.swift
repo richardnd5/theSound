a0b2dbc6d0482d4ab0ba1551aca0cf21d3fc45ad
N. Richard <nathan.d.richard@gmail.com>
2021-01-06 20:00:38 -0700

    added a set melody function.

--- theSound/MelodyPlayer.swift
+++ theSound/MelodyPlayer.swift
@@ -1,16 +1,13 @@
-//
-//  MelodyPlayer.swift
-//  theSound
-//
-//  Created by N Richard on 1/3/21.
-//
-
 import AudioKit
 
 public class MelodyPlayer {
 
 	var currentMelodyStep = 0
-	let melody = [1,1,5,5,6,6,5,4,4,3,3,2,2,1]
+	private var melody = [1,1,5,5,6,6,5,4,4,3,3,2,2,1]
+
+	func setMelody(_ melody: [Int]){
+		self.melody = melody
+	}
 
 	func scaleDegreeToMidiNote(_ degree: Int, key: Int = 0) -> MIDINoteNumber? {
 		if degree == 0 {
